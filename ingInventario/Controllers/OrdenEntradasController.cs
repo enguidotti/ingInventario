41a920827fbf15c30b9b3477f36cc0d393b5e322
eguidotti <enso.guidotti@gmail.com>
2021-06-25 19:51:18 -0400

    guardar detalle y actualziar stock

--- ingInventario/Controllers/OrdenEntradasController.cs
+++ ingInventario/Controllers/OrdenEntradasController.cs
@@ -1,6 +1,7 @@
 ﻿using ingInventario.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,16 +26,34 @@ namespace ingInventario.Controllers
             db.SaveChanges();
             //capturar el id_orden autoincrementable
             var idDet = orden.id_orden;
-            //continua con guardar los detalles
+            //se crea foreach para recorrer la lista de detalles(DetalleEntrada)
+            foreach (var item in detalles)
+            {
+                //se añade al registro de productos(detalle) el id_orden
+                item.id_orden = idDet;
+                db.DetalleEntrada.Add(item);
+                //actualizar stock del producto
+                ActualizaStock(item.id_producto, item.cantidad);
+                db.SaveChanges();
+            }
             return Json("");
+        }
+        //método para actualizar el stock existente de producto
+        public void ActualizaStock(int id_producto, int cantidad)
+        {
+            //verifica que el producto existe, según id
+            var producto = db.Producto.Find(id_producto);
+            //suma la nueva cantidad a la cantidad existente del producto
+            producto.cantidad = producto.cantidad + cantidad;//producto.cantidad += cantidad;
+            db.Entry(producto).State = EntityState.Modified;
         }
         [HttpPost]
         public ActionResult CodigoExiste(string codigo)
         {
             var q = db.Producto.FirstOrDefault(p => p.codigo.ToLower().Equals(codigo.ToLower()));
             if (q != null)
-            {                
-                return Json(new { q.nombre, q.id_producto },JsonRequestBehavior.AllowGet);
+            {
+                return Json(new { q.nombre, q.id_producto }, JsonRequestBehavior.AllowGet);
             }
             return Json("");
         }
--- ingInventario/Models/InventarioModel.Designer.cs
+++ ingInventario/Models/InventarioModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\workspace\ingInventario\ingInventario\Models\InventarioModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\Clases 2021-1\IG-DW\ingInventario\Models\InventarioModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
